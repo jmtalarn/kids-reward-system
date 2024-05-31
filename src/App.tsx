7f76e38b7d172c8fea04a43cc98875ca59c077e2
jmtalarn <jmtalarn@gmail.com>
2024-05-31 22:28:20 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+//import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import appLogo from '/favicon.svg'
+import PWABadge from './PWABadge.tsx'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>kids-reward-system</h1>
+
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
