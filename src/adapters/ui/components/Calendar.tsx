df60f1401e3fbcbb85647817ca7b50bddd305341
jmtalarn <jmtalarn@gmail.com>
2024-06-04 18:14:54 +0200

    Calendar

--- src/adapters/ui/components/Calendar.tsx
+++ src/adapters/ui/components/Calendar.tsx
@@ -1,21 +1,52 @@
 import { ReactNode, useState } from 'react';
 import style from './Calendar.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft, faTurnDown, faTurnLeftDown } from '@fortawesome/pro-duotone-svg-icons';
 const CalendarViewTypes = ["daily", "weekly", "monthly", "flow"];
 type CalendarViewType = typeof CalendarViewTypes[number];
 
+// reuse this https://l-u-k-e.medium.com/lets-build-a-full-page-calendar-with-react-fb6f99412e6a 
 
 interface ViewComponent { [key: CalendarViewType]: ReactNode }
 
-export const Calendar = () => {
-  const [view, setView] = useState<CalendarView>("daily");
 
-  const viewMap: ViewComponent = {
-    "daily": <h3>Daily</h3>, "weekly": <h3>Weekly</h3>, "monthly": <h3>Monthly</h3>, "flow": <h3>Flow</h3>
+function getDaysInMonth(month: number, year: number, fullGrid): Date[] {
+  const date = new Date(year, month, 1);
+  const days = [];
+  while (date.getMonth() === month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  if (fullGrid) {
+    return Array(days[0].getDay()).fill(null).concat(days).concat(Array(6 - days[days.length - 1].getDay()).fill(null));
+  }
+  else {
+    return days;
+  }
+}
+
+export const Calendar = ({ date }: { date: Date }) => {
+  const [view, setView] = useState<CalendarView>("daily");
+  const [dateSelected, setDateSelected] = useState<Date>(date || new Date());
+
+
+
+
+  const viewMap: ViewComponent =
+  {
+    "daily": () => <h3>Daily</h3>,
+    "weekly": () => <h3>Weekly</h3>,
+    "monthly": ({ dateSelected, onDayClick }) => (<>
+      <h3>Monthly</h3>
+      <MonthlyView dateSelected={dateSelected} onDayClick={onDayClick} />
+    </>),
+    "flow": ({ dateSelected, onDayClick }) => <><h3>Flow</h3><FlowView dateSelected={dateSelected} onDayClick={onDayClick} /></>
   };
   return <div>
-    <header>
+    <header className={style.header}>
+      <h2>{dateSelected.toDateString()}</h2>
       <select
+        value={view}
         onChange={(evt) => setView(evt.target.value)}
       >
         {CalendarViewTypes
@@ -23,8 +54,46 @@ export const Calendar = () => {
       </select>
     </header>
     <div className={style.calendar}>
-      {viewMap[view]}
+      {viewMap[view]({ dateSelected, onDayClick: (date) => { setDateSelected(date); setView("daily"); } })}
+
     </div>
   </div>
 
 };
+
+const MonthlyView = ({ dateSelected, onDayClick }: { dateSelected: Date, onDayClick: () => void }) => {
+
+  const days = getDaysInMonth(dateSelected.getMonth(), dateSelected.getFullYear(), true);
+  return <div className={style['calendar-grid-month']}>
+    {
+      days.map((day, idx) => (<span
+        className={style.day}
+        key={`${idx}_${day?.getDay() || "Empty"}`}
+        onClick={() => day && onDayClick(day)}
+      >
+        {day?.getDate() || ""}
+      </span>
+      )
+      )
+    }
+  </div>
+}
+
+const FlowView = ({ dateSelected, onDayClick }: { dateSelected: Date, onDayClick: () => void }) => {
+  const days = getDaysInMonth(dateSelected.getMonth(), dateSelected.getFullYear(), false);
+  return <div className={style['calendar-grid-flow']}>
+    {
+      days.map((day, idx) => (<><span
+        className={`${style['flow-day']} ${style.day}`}
+        key={`${idx}_${day?.getDay() || "Empty"}`}
+        onClick={() => day && onDayClick(day)}
+      >
+        {day?.getDate() || ""}
+      </span>
+        {idx !== days.length - 1 && <span className={style.arrow}><FontAwesomeIcon icon={faArrowRight} /></span>}
+      </>
+      )
+      )
+    }
+  </div>
+}
