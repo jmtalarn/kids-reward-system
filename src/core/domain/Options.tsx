694e69fa0246d17f9fb80cdcbab6decab309d227
jmtalarn <jmtalarn@gmail.com>
2024-09-11 17:05:05 +0200

    Also icons

--- src/core/domain/Options.tsx
+++ src/core/domain/Options.tsx
@@ -1,9 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFaceAwesome, faFaceVomit, faFaceThinking } from '@fortawesome/pro-duotone-svg-icons';
-
+import Icon from '../../adapters/ui/components/Icon'
 
 export const options = [
-	{ option: <FontAwesomeIcon icon={faFaceAwesome} style={{ color: 'lime' }} />, value: 3 },
-	{ option: <FontAwesomeIcon icon={faFaceThinking} style={{ color: 'gold' }} />, value: 1 },
-	{ option: <FontAwesomeIcon icon={faFaceVomit} style={{ color: 'salmon' }} />, value: 0 },
+	{ option: <Icon icon="smile" style={{ color: 'lime' }} />, value: 3 },
+	{ option: <Icon icon="meh" style={{ color: 'gold' }} />, value: 1 },
+	{ option: <Icon icon="frown" style={{ color: 'salmon' }} />, value: 0 },
 ];
