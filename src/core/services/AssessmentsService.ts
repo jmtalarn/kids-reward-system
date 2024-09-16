beaa0a6113990a0de8e26851494d2758f0a514af
jmtalarn <jmtalarn@gmail.com>
2024-09-16 13:16:19 +0200

    Assessment

--- src/core/services/AssessmentsService.ts
+++ src/core/services/AssessmentsService.ts
@@ -4,10 +4,10 @@ import { TaskId } from '../domain/Task';
 import { Assessment } from '../domain/Assessment';
 
 const addAssessment = async ({ date, participantId, taskId, option }: { date: Date, participantId: ParticipantId, taskId: TaskId, option: DialogOption }): Promise<Assessment[]> => {
-  return assessmentsRepository.addAssessment(date, participantId, taskId, option);
+  return assessmentsRepository.addAssessment({ date, participantId, taskId, option });
 }
 const removeAssessment = async ({ date, participantId, taskId }: { date: Date, participantId: Participant, taskId: Task }): Promise<Assessment[]> => {
-  return assessmentsRepository.removeAssessment(date, participantId, taskId);
+  return assessmentsRepository.removeAssessment({ date, participantId, taskId });
 }
 
 const getAllAssessments = async (): Promise<Assessment[]> => {
--- src/infrastructure/repositories/assessments/localStorage.ts
+++ src/infrastructure/repositories/assessments/localStorage.ts
@@ -3,12 +3,17 @@ const LOCAL_STORAGE_KEY = 'KRS_ASSESSMENTS';
 
 const getAllAssessments = async (): Promise<Assessment[]> => {
 	const assessments = localStorage.getItem(LOCAL_STORAGE_KEY);
-	return assessments ? JSON.parse(assessments) : [];
+	return assessments ? JSON.parse(assessments) : {};
 };
 
 const addAssessment = async ({ date, participantId, taskId, option }: { date: Date, participantId: ParticipantId, taskId: TaskId, option: DialogOption }): Promise<Assessment[]> => {
 	const assessments = await getAllAssessments();
-
+	if (!assessments[date]) {
+		assessments[date] = {};
+	}
+	if (!assessments[date][participantId]) {
+		assessments[date][participantId] = {};
+	}
 	assessments[date][participantId] = { ...assessments[date][participantId], [taskId]: option };
 
 	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(assessments));
